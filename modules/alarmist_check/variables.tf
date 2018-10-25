f2d2a128f1aece28d9815293d9b9d765d7cf220b
Adam Panzer <adam.panzer@optimizely.com>
2018-10-25 10:33:06 -0700

    Add actions

--- modules/alarmist_check/variables.tf
+++ modules/alarmist_check/variables.tf
@@ -4,3 +4,9 @@ variable "alarmist_short_name" {}
 variable "alarmist_path" {
   default = "/"
 }
+
+variable "alarmist_action_arns" {
+  type = "string"
+  description = "Comma seperated list of arns to action on when moved to failed state."
+  default = ""
+}
